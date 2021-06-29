726789971800b045750021c9363254215dfe51b1
Dashup <git@dashup.io>
2021-06-29 11:53:39 +1000

    react logic

--- views/block/document.tsx
+++ views/block/document.tsx
@@ -1,9 +1,85 @@
 
 // import react
+import shortid from 'shortid';
+import EditorJs from 'react-editor-js';
 import React from 'react';
+
+// import base
+import Link from '@editorjs/link';
+import Code from '@editorjs/code';
+import List from '@editorjs/list';
+import Table from '@editorjs/table';
+import Quote from '@editorjs/quote';
+import Image from '@editorjs/image';
+import Embed from '@editorjs/embed';
+import Marker from '@editorjs/marker';
+import Header from '@editorjs/header';
+import Warning from '@editorjs/warning';
+import Underline from '@editorjs/underline';
+import Delimiter from '@editorjs/delimiter';
+
+// global timer
+let timer;
+
+// global debounce
+const debounce = (func, timeout = 1000) => {
+
+  // return debounced
+  return (...args) => {
+    // clear timeout previously
+    clearTimeout(timer);
+
+    // create new timeout
+    timer = setTimeout(() => func(...args), timeout);
+  };
+}
 
 // create chart block
 const BlockDocument = (props = {}) => {
+
+  // upload by url
+  const uploadByUrl = async (url) => {
+    // return uploaded
+    return {
+      file : {
+        url,
+      },
+      success : 1,
+    };
+  };
+
+  // upload by file
+  const uploadByFile = async (image) => {
+    // loop image
+    if (!image) return;
+
+    // create form data
+    const data = new FormData();
+
+    // append image
+    data.append('file', image);
+    data.append('temp', shortid());
+
+    // submit ajax form
+    const res = await fetch('/media/image', {
+      body   : data,
+      method : 'POST',
+    });
+
+    // await json
+    const result = await res.json();
+
+    // upload
+    const { upload } = result;
+
+    // return uploaded
+    return {
+      file : {
+        url : upload.url,
+      },
+      success : 1,
+    };
+  };
 
   // return jsx
   return (
@@ -13,8 +89,50 @@ const BlockDocument = (props = {}) => {
           { props.block.label }
         </div>
       ) }
-      <div className="card-body p-0 d-flex">
-        EDITOR TODO
+      <div className="card-body">
+        <div className="w-100 h-100 fit-content">
+          <EditorJs
+            tools={ {
+              code      : Code,
+              link      : Link,
+              list      : List,
+              quote     : Quote,
+              table     : Table,
+              header    : Header,
+              marker    : Marker,
+              warning   : Warning,
+              underline : Underline,
+              delimiter : Delimiter,
+              embed : {
+                class  : Embed,
+                config : {
+                  services : {
+                    coub    : true,
+                    youtube : true,
+                  }
+                },
+                inlineToolbar : true
+              },
+              image : {
+                class  : Image,
+                config : {
+                  uploader : {
+                    uploadByUrl,
+                    uploadByFile,
+                  },
+                }
+              }
+            } }
+            data={ typeof props.block.data === 'string' ? JSON.parse(props.block.data) : {} }
+            onChange={ debounce(async (editor) => {
+              // data
+              const data = await editor.saver.save();
+
+              // save
+              debounce(props.setBlock)(props.block, 'data', JSON.stringify(data));
+            }, 100) }
+          />
+        </div>
       </div>
     </div>
   );
--- views/page/document.tsx
+++ views/page/document.tsx
@@ -1,14 +1,92 @@
 
 // import react
 import { Page } from '@dashup/ui';
+import shortid from 'shortid';
+import EditorJs from 'react-editor-js';
 import React, { useState } from 'react';
+
+// import base
+import Link from '@editorjs/link';
+import Code from '@editorjs/code';
+import html from 'editorjs-html';
+import List from '@editorjs/list';
+import Table from '@editorjs/table';
+import Quote from '@editorjs/quote';
+import Image from '@editorjs/image';
+import Embed from '@editorjs/embed';
+import Marker from '@editorjs/marker';
+import Header from '@editorjs/header';
+import Warning from '@editorjs/warning';
+import Underline from '@editorjs/underline';
+import Delimiter from '@editorjs/delimiter';
+
+// global timer
+let timer;
+
+// global debounce
+const debounce = (func, timeout = 1000) => {
+
+  // return debounced
+  return (...args) => {
+    // clear timeout previously
+    clearTimeout(timer);
+
+    // create new timeout
+    timer = setTimeout(() => func(...args), timeout);
+  };
+}
 
 // create chart block
 const PageDocument = (props = {}) => {
   // groups
   const [share, setShare] = useState(false);
   const [config, setConfig] = useState(false);
+  const [parser, setParser] = useState(html());
   const [updating, setUpdating] = useState(false);
+
+  // upload by url
+  const uploadByUrl = async (url) => {
+    // return uploaded
+    return {
+      file : {
+        url,
+      },
+      success : 1,
+    };
+  };
+
+  // upload by file
+  const uploadByFile = async (image) => {
+    // loop image
+    if (!image) return;
+
+    // create form data
+    const data = new FormData();
+
+    // append image
+    data.append('file', image);
+    data.append('temp', shortid());
+
+    // submit ajax form
+    const res = await fetch('/media/image', {
+      body   : data,
+      method : 'POST',
+    });
+
+    // await json
+    const result = await res.json();
+
+    // upload
+    const { upload } = result;
+
+    // return uploaded
+    return {
+      file : {
+        url : upload.url,
+      },
+      success : 1,
+    };
+  };
 
   // return jsx
   return (
@@ -26,7 +104,60 @@ const PageDocument = (props = {}) => {
         ) }
       </Page.Menu>
       <Page.Body>
-        EDITOR
+        { updating ? (
+          <div className="flex-1 d-flex fit-content">
+            <EditorJs
+              tools={ {
+                code      : Code,
+                link      : Link,
+                list      : List,
+                quote     : Quote,
+                table     : Table,
+                header    : Header,
+                marker    : Marker,
+                warning   : Warning,
+                underline : Underline,
+                delimiter : Delimiter,
+                embed : {
+                  class  : Embed,
+                  config : {
+                    services : {
+                      coub    : true,
+                      youtube : true,
+                    }
+                  },
+                  inlineToolbar : true
+                },
+                image : {
+                  class  : Image,
+                  config : {
+                    uploader : {
+                      uploadByUrl,
+                      uploadByFile,
+                    },
+                  }
+                }
+              } }
+              data={ typeof props.page.get('data.data') === 'string' ? JSON.parse(props.page.get('data.data')) : {} }
+              onReady={ (editor) => {
+                // check data
+                if (!props.page.get('data.data') && props.page.get('data.document')) editor.blocks.renderFromHTML(props.page.get('data.document') || '');
+              } }
+              onChange={ debounce(async (editor) => {
+                // data
+                const data = await editor.saver.save();
+
+                // save
+                debounce(props.setData)({
+                  data : JSON.stringify(data),
+                  html : parser.parse(data),
+                });
+              }, 100) }
+            />
+          </div>
+        ) : (
+          <div className="container" dangerouslySetInnerHTML={ { __html : (props.page.get('data.html') ? props.page.get('data.html').join('') : null) || props.page.get('data.document') || '' } } />
+        ) }
       </Page.Body>
     </Page>
   );
